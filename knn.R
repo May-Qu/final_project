541fa60ea13ef357d0378db5cdd56b12ea7b1d15
xz433 <zouxi9606@163.com>
2020-05-04 04:19:17 -0400

    regression model

--- knn.R
+++ knn.R
@@ -60,4 +60,82 @@ prediction %>%
 
 
 
+# knn & cart regression model
+hr <- happiness %>%
+  select(-ID)
+
+sr <- initial_split(data = hr, prop = 0.8)
+h_training <- training(sr)
+h_testing <- testing(sr)
+
+h_resamples <- vfold_cv(data = hr, v = 10)
+
+train_imputation <- function(split, formula, model, ...) {
+  analysis_data <- analysis(sr)
+  if (model == "knn") {
+    model <-
+    nearest_neighbor(mode = "regression", 
+                     neighbors = 5) %>%
+      set_engine("kknn") %>%
+      fit(formula, data = analysis_data)
+  } else if (model == "cart") {
+    model <-
+      decision_tree(mode = "regression") %>%
+      set_engine("rpart") %>%
+      fit(formula, data = analysis_data)
+  }
+  assessment_data <- assessment(sr)
+  rmse <- bind_cols(
+    assessment_data,
+    predict(model, assessment_data)
+  ) %>%
+    rmse(truth = index, estimate = .pred) %>%
+    pull(.estimate)
+  return(rmse)
+}
+
+h_resamples <- h_resamples %>%
+  mutate(knn_rmse = map_dbl(.x = splits,
+                            .f = ~train_imputation(split = .x,
+                                              formula = index ~ . ,
+                                              model = "knn")))
+
+h_resamples <- h_resamples %>%
+  mutate(cart_rmse = map_dbl(.x = splits,
+                            .f = ~train_imputation(split = .x,
+                                                   formula = index ~ . ,
+                                                   model = "cart")))
+h_resamples <- h_resamples %>%
+  select(-splits)
+
+knitr::kable(h_resamples)
+
+h_resamples %>% 
+  pivot_longer(-id) %>%
+  ggplot(aes(x = id,
+             y = value,
+             color = name, group = name)) +
+  geom_point() +
+  geom_line() + 
+  scale_y_continuous(limits = c(0, NA))
+    
+hm <-
+  decision_tree(mode = "regression") %>%
+  set_engine("rpart") %>%
+  fit(index ~ . ,
+      data = h_training)
+
+bind_cols(
+  h_testing,
+  predict(hm, h_testing) 
+) %>%
+  metrics(truth = index, estimate = .pred)
+
+
+
+
+
+
+
+
 
